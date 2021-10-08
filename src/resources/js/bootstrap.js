004d1f4ee80670991f9a0a7ef517a80b3b056b7a
swkidd <kiddsoftware@gmail.com>
2021-10-08 11:21:18 +0900

    Add Backbone.js and bootstrap CSS

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -1,5 +1,6 @@
 window._ = require('lodash');
-
+window.$ = window.jQuery = require('jquery');
+window.Backbone = require('backbone');
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
